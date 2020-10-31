24263f8c5fc74d2779cab016fc4956e3607f6122
SamSepi0l <Triton.halley@outlook.com>
2020-10-31 10:46:45 +0330

    RecyclerView adapter and holder and layout build with DataBinding

--- app/src/main/java/com/tritonhalley/tritonhalleystore/adapters/Adapter.java
+++ app/src/main/java/com/tritonhalley/tritonhalleystore/adapters/Adapter.java
@@ -1,33 +1,68 @@
 package com.tritonhalley.tritonhalleystore.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.tritonhalley.tritonhalleystore.R;
+import com.tritonhalley.tritonhalleystore.data.model.Product;
+import com.tritonhalley.tritonhalleystore.databinding.ListItemsBinding;
+
+import java.util.List;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.ProductHolder> {
+    private List<Product> products;
+    private Context mContext;
+
+    public Adapter(List<Product> products, Context context) {
+        this.products = products;
+        mContext = context;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 
     @NonNull
     @Override
     public ProductHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        ListItemsBinding listItems = DataBindingUtil.inflate(
+                LayoutInflater.from(mContext),
+                R.layout.list_items,
+                parent,
+                false);
+        return new ProductHolder(listItems);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProductHolder holder, int position) {
-
+        holder.bindItem(products.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return products.size();
     }
 
     class ProductHolder extends RecyclerView.ViewHolder {
+        private ListItemsBinding mListItemsBinding;
+        private Product mProduct;
+        public ProductHolder(ListItemsBinding listItems) {
+            super(listItems.getRoot());
+            mListItemsBinding = listItems;
+        }
+        public void bindItem(Product product){
+            mProduct = product;
 
-        public ProductHolder(@NonNull View itemView) {
-            super(itemView);
         }
     }
 }
